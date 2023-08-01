80449109cb31684603d99732ccfec8804883e733
Dell <Dell@DESKTOP-2Q93AO7>
2023-08-01 17:45:41 +0300

    Add project files.

---
+++ WebUygulamaProje1/Controllers/KitapTuruController.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebUygulamaProje1.Models;
+using WebUygulamaProje1.Utility;
+
+namespace WebUygulamaProje1.Controllers
+{
+    public class KitapTuruController : Controller
+    {
+        private readonly UygulamaDbContext _uygulamaDbContext;
+        public KitapTuruController (UygulamaDbContext context)
+        {
+            _uygulamaDbContext = context;
+        }
+        public IActionResult Index()
+        {
+            List<KitapTuru> objKitapTuruList  = _uygulamaDbContext.KitapTurleri.ToList();
+            return View(objKitapTuruList);
+        }
+
+        public IActionResult Ekle() {
+             return View();
+        }
+        [HttpPost]
+		public IActionResult Ekle(KitapTuru KitapTuru)
+		{
+
+			
+			 if (ModelState.IsValid)
+			  {
+				  _uygulamaDbContext.KitapTurleri.Add(KitapTuru);
+				  _uygulamaDbContext.SaveChanges();//ave changes yapılmazsa bilgiler veri tabanına eklenmez!!!!
+				  return RedirectToAction("Index");
+			  }
+
+
+				  return View();
+
+
+		}
+		public IActionResult Guncelle(int? id)
+		{
+			if(id==null || id == 0)
+			{
+				return NotFound();
+			}
+			KitapTuru? kitapTuruVt=_uygulamaDbContext.KitapTurleri.Find(id);
+			if (kitapTuruVt == null)
+			{
+				return NotFound();
+			}
+
+			return View(kitapTuruVt);
+		}
+		[HttpPost]
+		public IActionResult Guncelle(KitapTuru KitapTuru)
+		{
+
+
+			if (ModelState.IsValid)
+			{
+				_uygulamaDbContext.KitapTurleri.Update(KitapTuru);
+				_uygulamaDbContext.SaveChanges();//save changes yapılmazsa bilgiler veri tabanına eklenmez!!!!
+				return RedirectToAction("Index");
+			}
+
+
+			return View();
+
+
+		}
+		//get action .cshtml dosyasını getirir.
+		public IActionResult Sil(int? id)
+		{
+			if (id == null || id == 0)
+			{
+				return NotFound();
+			}
+			KitapTuru? kitapTuruVt = _uygulamaDbContext.KitapTurleri.Find(id);
+			if (kitapTuruVt == null)
+			{
+				return NotFound();
+			}
+
+			return View(kitapTuruVt);
+		}
+		//post action.asıl yapılmasını istediğimiz işlemi yapar.
+		[HttpPost,ActionName("Sil")]
+		public IActionResult SilPOST(int? id)
+		{
+
+
+			if (ModelState.IsValid)
+			{
+				KitapTuru? KitapTuru =_uygulamaDbContext.KitapTurleri.Find(id);
+				if(KitapTuru==null)
+				{
+					return NotFound();
+				}
+				_uygulamaDbContext.KitapTurleri.Remove(KitapTuru);
+				_uygulamaDbContext.SaveChanges();
+				return RedirectToAction("Index");
+			}
+
+
+			return View();
+
+
+		}
+	}
+}
---
+++ WebUygulamaProje1/Models/KitapTuru.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebUygulamaProje1.Models
+{
+    public class KitapTuru
+    {
+        [Key]   //set primary key anlamına gelir .Eğer id  yi "Id" şeklinde tanımlarsan  [Key] kısmını yazmasanda microsoft primary key olarak aLgılar .
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Kitap Tür Adı Boş Bırakılamaz!")]  //not null anlamına gelir
+        [MaxLength(25)]
+        [DisplayName("Kitap Türü Adı")]
+        public string Ad { get; set; }
+
+
+    }
+}
